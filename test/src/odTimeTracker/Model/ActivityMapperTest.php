502cd57cf3c2baf11f02cb88a41c8d6eafc2af4d
ondrejd <ondrejd@gmail.com>
2015-08-10 02:22:49 +0200

    Prepared tests for ActivityMapper.

---
+++ test/src/odTimeTracker/Model/ActivityMapperTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * odtimetracker-php-cgi
+ *
+ * @license Mozilla Public License 2.0 https://www.mozilla.org/MPL/2.0/
+ * @author Ondřej Doněk, <ondrejd@gmail.com>
+ */
+namespace odTimeTracker\Model;
+
+use PHPUnit_Framework_TestCase;
+
+class ActivityMapperTest extends PHPUnit_Framework_TestCase
+{
+	public function testSelectRecentActivities()
+	{
+		$this->markTestIncomplete();
+	}
+
+	public function testSelectRunningActivity()
+	{
+		$this->markTestIncomplete();
+	}
+
+	public function testSelectActivitiesForInterval()
+	{
+		$this->markTestIncomplete();
+	}
+
+	public function testStartActivity()
+	{
+		$this->markTestIncomplete();
+	}
+
+	public function testStopRunningActivity()
+	{
+		$this->markTestIncomplete();
+	}
+}
