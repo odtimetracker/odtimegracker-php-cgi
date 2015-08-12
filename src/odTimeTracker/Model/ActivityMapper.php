32168bdd009835205708eed6537aebc61e784344
ondrejd <ondrejd@gmail.com>
2015-08-12 20:36:51 +0200

    Added ProjectMapper. Updated entities, mappers and corresponding tests.

--- src/odTimeTracker/Model/ActivityMapper.php
+++ src/odTimeTracker/Model/ActivityMapper.php
@@ -10,8 +10,117 @@ namespace odTimeTracker\Model;
 /**
  * Mapper for activities.
  */
-class ActivityMapper extends CommonMapper
+class ActivityMapper extends AbstractMapper
 {
+	/**
+	 * @const string Database table name.
+	 */
+	const TABLE_NAME = 'Activities';
+
+	/**
+	 * @const string Primary key column name.
+	 */
+	const PK_COL_NAME = 'ActivityId';
+
+	/**
+	 * Create schema.
+	 *
+	 * @return boolean
+	 */
+	function createSchema()
+	{
+		$table = self::TABLE_NAME;
+		$stmt = $this->db->getPdo()->prepare(<<<EOD
+CREATE TABLE IF NOT EXISTS `$table` (
+	ActivityId INTEGER PRIMARY KEY,
+	ProjectId INTEGER NOT NULL,
+	Name TEXT,
+	Description TEXT,
+	Tags TEXT,
+	Started TEXT NOT NULL,
+	Stopped TEXT NOT NULL DEFAULT '',
+	FOREIGN KEY(ProjectId) REFERENCES Projects(ProjectId) 
+)
+EOD
+		);
+
+		return $stmt->execute();
+	}
+
+	/**
+	 * Insert new record.
+	 *
+	 * @param \odTimeTracker\Model\ActivityEntity|array $data
+	 * @return \odTimeTracker\Model\ActivityEntity|boolean Returns `FALSE` if anything goes wrong.
+	 */
+	function insert($entity)
+	{
+		if (!is_array($entity) && !($entity instanceof ActivityEntity)) {
+			return false;
+		}
+
+		$projectId = is_array($entity) ? $entity['ProjectId'] : $entity->getProjectId();
+		$name = is_array($entity) ? $entity['Name'] : $entity->getName();
+		$desc = is_array($entity) ? (array_key_exists('Description', $entity) ? $entity['Description'] : '') : $entity->getDescription();
+		$tags = is_array($entity) ? (array_key_exists('Tags', $entity) ? $entity['Tags'] : '') : $entity->getTags();
+		$started = is_array($entity) ? (array_key_exists('Started', $entity) ? $entity['Started'] : null) : $entity->getStarted();
+		$startedObj = is_null($started) ? new \DateTime() : (($started instanceof \DateTime) ? $started : new \DateTime($started));
+		$startedStr = $startedObj->format(\DateTime::RFC3339);
+		$stopped = is_array($entity) ? (array_key_exists('Stopped', $entity) ? $entity['Stopped'] : null) : $entity->getStopped();
+		$stoppedObj = is_null($stopped) ? null : (($stopped instanceof \DateTime) ? $stopped : new \DateTime($stopped));
+		$stoppedStr = ($stopped instanceof \DateTime) ? $stoppedObj->format(\DateTime::RFC3339) : '';
+
+		$table = self::TABLE_NAME;
+		$stmt = $this->db->getPdo()->prepare(<<<EOD
+INSERT INTO `$table` (`ProjectId`, `Name`, `Description`, `Tags`, `Started`, `Stopped`) 
+VALUES ( :projectId, :name , :desc , :tags , :started , :stopped );
+EOD
+		);
+		$stmt->bindParam(':projectId', $projectId, \PDO::PARAM_INT);
+		$stmt->bindParam(':name', $name, \PDO::PARAM_STR);
+		$stmt->bindParam(':desc', $desc, \PDO::PARAM_STR);
+		$stmt->bindParam(':tags', $tags, \PDO::PARAM_STR);
+		$stmt->bindParam(':started', $startedStr, \PDO::PARAM_STR);
+		$stmt->bindParam(':stopped', $stoppedStr);
+		$res = $stmt->execute();
+		//var_dump($stmt->debugDumpParams());exit();
+
+		if ($res === false) {
+			return false;
+		}
+
+		return new ActivityEntity(array(
+			'ProjectId' => $this->db->getPdo()->lastInsertId(),
+			'Name' => $name,
+			'Description' => empty($description) ? null : $description,
+			'Tags' => empty($tags) ? null : $tags,
+			'Started' => $started,
+			'Stopped' => $stopped
+		));
+	}
+
+	/**
+	 * Update record.
+	 *
+	 * @param \odTimeTracker\Model\ActivityEntity $entity
+	 * @return \odTimeTracker\Model\ActivityEntity|boolean Returns `FALSE` if anything goes wrong.
+	 */
+	function update($entity)
+	{
+		// ...
+	}
+
+	/**
+	 * Select all records.
+	 *
+	 * @param integer $limit (Optional.)
+	 * @return array Returns array of {@see \odTimeTracker\Model\ActivityEntity}.
+	 */
+	function selectAll($limit = 0)
+	{
+		// ...
+	}
+
 	/**
 	 * Select recent activities.
 	 *
@@ -20,6 +129,7 @@ class ActivityMapper extends CommonMapper
 	 */
 	public function selectRecentActivities($limit)
 	{
+		$table = self::TABLE_NAME;
 		$stmt = $this->db->getPdo()->prepare(<<<EOD
 SELECT 
 	`t1`.*,
@@ -27,7 +137,7 @@ SELECT
 	`t2`.`Name` AS `Project.Name`,
 	`t2`.`Description` AS `Project.Description`,
 	`t2`.`Created` AS `Project.Created`   
-FROM `Activities` AS `t1` 
+FROM `$table` AS `t1` 
 LEFT JOIN `Projects` AS `t2` ON `t1`.`ProjectId` = `t2`.`ProjectId` 
 ORDER BY `t1`.`Started` DESC 
 LIMIT :limit ;
@@ -57,6 +167,7 @@ EOD
 	 */
 	public function selectRunningActivity()
 	{
+		$table = self::TABLE_NAME;
 		$stmt = $this->db->getPdo()->prepare(<<<EOD
 SELECT 
 	`t1`.*,
@@ -64,7 +175,7 @@ SELECT
 	`t2`.`Name` AS `Project.Name`,
 	`t2`.`Description` AS `Project.Description`,
 	`t2`.`Created` AS `Project.Created`   
-FROM `Activities` AS `t1` 
+FROM `$table` AS `t1` 
 LEFT JOIN `Projects` AS `t2` ON `t1`.`ProjectId` = `t2`.`ProjectId` 
 WHERE `t1`.`Stopped` IS NULL OR `t1`.`Stopped` = '' 
 LIMIT 1 ;
@@ -94,6 +205,7 @@ EOD
 	 */
 	public function selectActivitiesForInterval($dateFrom, $dateTo)
 	{
+		$table = self::TABLE_NAME;
 		$stmt = $this->db->getPdo()->prepare(<<<EOD
 SELECT 
 	`t1`.*,
@@ -101,13 +213,12 @@ SELECT
 	`t2`.`Name` AS `Project.Name`,
 	`t2`.`Description` AS `Project.Description`,
 	`t2`.`Created` AS `Project.Created`   
-FROM `Activities` AS `t1` 
+FROM `$table` AS `t1` 
 LEFT JOIN `Projects` AS `t2` ON `t1`.`ProjectId` = `t2`.`ProjectId` 
 WHERE `t1`.`Started` > :dateFrom AND `t1`.`Started` < :dateTo 
 ORDER BY `t1`.`Started` DESC ;
 EOD
 		);
-
 		$stmt->bindParam(':dateFrom', $dateFrom, \PDO::PARAM_STR);
 		$stmt->bindParam(':dateTo', $dateTo, \PDO::PARAM_STR);
 		$res = $stmt->execute();
@@ -147,12 +258,12 @@ EOD
 		$started = new \DateTime();
 		$startedStr = $started->format(\DateTime::RFC3339);
 
+		$table = self::TABLE_NAME;
 		$stmt = $this->db->getPdo()->prepare(<<<EOD
-INSERT INTO `Activities` (`ProjectId`, `Name`, `Description`, `Tags`, `Started`) 
+INSERT INTO `$table` (`ProjectId`, `Name`, `Description`, `Tags`, `Started`) 
 VALUES ( :projectId , :name , :description , :tags, :started );
 EOD
 		);
-
 		$stmt->bindParam(':projectId', $projectId, \PDO::PARAM_INT);
 		$stmt->bindParam(':name', $name, \PDO::PARAM_STR);
 		$stmt->bindParam(':description', $description, \PDO::PARAM_STR);
@@ -188,17 +299,17 @@ EOD
 			return false;
 		}
 
-$stmt = $this->db->getPdo()->prepare(<<<EOD
-UPDATE `Activities` 
-SET `Stopped` = :stopped 
-WHERE `ActivityId` = :activityId ;
-EOD
-		);
-
 		$nowObj = new \DateTime('now');
 		$nowStr = $nowObj->format(\DateTime::RFC3339);
 		$activityId = $runningActivity->getActivityId();
 
+		$table = self::TABLE_NAME;
+		$stmt = $this->db->getPdo()->prepare(<<<EOD
+UPDATE `$table` 
+SET `Stopped` = :stopped 
+WHERE `ActivityId` = :activityId ;
+EOD
+		);
 		$stmt->bindParam(':stopped', $nowStr, \PDO::PARAM_STR);
 		$stmt->bindParam(':activityId', $activityId, \PDO::PARAM_INT);
 		$res = $stmt->execute();
--- test/src/odTimeTracker/Model/ActivityMapperTest.php
+++ test/src/odTimeTracker/Model/ActivityMapperTest.php
@@ -9,29 +9,145 @@ namespace odTimeTracker\Model;
 
 use PHPUnit_Framework_TestCase;
 
+/**
+ * Tests for {@see \odTimeTracker\Model\ActivityMapper} using SQLite as a database.
+ */
 class ActivityMapperTest extends PHPUnit_Framework_TestCase
 {
-	public function testSelectRecentActivities()
+	/**
+	 * Test mapper on SQLite database.
+	 */
+	public function testSqlite()
 	{
+		try {
+			$pdo = new \odTimeTracker\Db\MyPdo('sqlite::memory:');
+
+			if (!($pdo instanceof \odTimeTracker\Db\MyPdo)) {
+				throw new \Exception('MyPdo was not initialized!');
+			}
+		} catch (\Exception $e) {
+			$this->markTestSkipped('Database connection was not established!');
+		}
+
+		$mapper = new ActivityMapper($pdo);
+		$projectMapper = new ProjectMapper($pdo);
+		$projectRes = $projectMapper->createSchema();
+		$this->assertTrue($projectRes);
+		$project1 = $projectMapper->insert(new ProjectEntity(array(
+			'Name' => 'Project #1', 
+			'Description' => 'The first test project.'
+		)));
+		$this->assertInstanceOf('\odTimeTracker\Model\ProjectEntity', $project1);
+
+		// Test `createSchema`
+		$res = $mapper->createSchema();
+		$this->assertTrue($res);
+
+		if (!$mapper->createSchema()) {
+			$this->markTestSkipped('Database schema was not created!');
+		}
+
+		// Test `insert`
+		$activity1 = $mapper->insert(new ActivityEntity(array(
+			'ProjectId' => 1,
+			'Name' => 'Activity #1',
+			'Description' => 'The first test activity.',
+			'Tags' => 'tag1,tag2,tag3',
+			'Started' => '2011-10-05 10:00:00.0000+1:00',
+			'Stopped' => '2011-10-05 13:00:00.0000+1:00'
+		)));
+		$this->assertInstanceOf('\odTimeTracker\Model\ActivityEntity', $activity1);
+
+		$activity2 = $mapper->insert(new ActivityEntity(array(
+			'ProjectId' => 1,
+			'Name' => 'Activity #2',
+			'Description' => 'The second test activity.',
+			'Tags' => 'tag2,tag3',
+			'Started' => '2011-10-05 14:10:00.0000+1:00',
+			'Stopped' => '2011-10-05 21:15:30.0000+1:00'
+		)));
+		$this->assertInstanceOf('\odTimeTracker\Model\ActivityEntity', $activity2);
+
+		$activity3 = $mapper->insert(new ActivityEntity(array(
+			'ProjectId' => 1,
+			'Name' => 'Activity #3',
+			'Description' => 'The third test activity.',
+			'Tags' => 'tag3,tag5',
+			'Started' => '2011-10-06 07:15:00.0000+1:00',
+			'Stopped' => '2011-10-06 19:45:00.0000+1:00'
+		)));
+		$this->assertInstanceOf('\odTimeTracker\Model\ActivityEntity', $activity3);
+
+		$activity4 = $mapper->insert(new ActivityEntity(array(
+			'ProjectId' => 1,
+			'Name' => 'Activity #4',
+			'Description' => 'The fourth test activity.',
+			'Tags' => null,
+			'Started' => '2011-10-07 09:15:00.0000+1:00',
+			'Stopped' => '2011-10-07 09:19:00.0000+1:00'
+		)));
+		$this->assertInstanceOf('\odTimeTracker\Model\ActivityEntity', $activity4);
+
+		$activity5 = $mapper->insert(new ActivityEntity(array(
+			'ProjectId' => 1,
+			'Name' => 'Activity #5',
+			'Description' => null,
+			'Tags' => '',
+			'Started' => '2011-10-07 10:01:10.0000+1:00',
+			'Stopped' => '2011-10-07 11:32:55.0000+1:00'
+		)));
+		$this->assertInstanceOf('\odTimeTracker\Model\ActivityEntity', $activity5);
+
+		$activity6 = $mapper->insert(array(
+			'ProjectId' => 1,
+			'Name' => 'Activity #6', 
+			'Description' => 'The sixth test activity.',
+			'Tags' => 'tag3,tag4',
+			'Started' => '2011-10-07 12:00:00.0000+1:00',
+			'Stopped' => '2011-10-08 00:30:00.0000+1:00'
+		));
+		$this->assertInstanceOf('\odTimeTracker\Model\ActivityEntity', $activity6);
+
+		$activity7 = $mapper->insert(array(
+			'ProjectId' => 1,
+			'Name' => 'Activity #7', 
+			'Description' => 'The seventh test activity.',
+			'Tags' => 'tag1',
+			'Started' => '2011-10-08 12:00:00.0000+1:00',
+			'Stopped' => '2011-10-08 13:05:00.0000+1:00'
+		));
+		$this->assertInstanceOf('\odTimeTracker\Model\ActivityEntity', $activity7);
+
+		$activity8 = $mapper->insert(array(
+			'ProjectId' => 1,
+			'Name' => 'Activity #8', 
+			'Started' => '2011-10-09 18:00:00.0000+1:00',
+			'Stopped' => '2011-10-09 19:45:00.0000+1:00'
+		));
+		$this->assertInstanceOf('\odTimeTracker\Model\ActivityEntity', $activity8);
+
+		// Test `selectAll`
+		//$results = $mapper->selectAll();
+		//$this->assertEquals(8, count($results));
+
+		// Test "random" activity
+		/*$testActivity = $results[4];
+		$this->assertGreaterThanOrEqual(5, $testActivity->getId());
+		$this->assertGreaterThanOrEqual(5, $testActivity->getActivityId());
+		$this->assertEquals('Activity #5', $testActivity->getName());
+		$this->assertEquals('The fifth test activity.', $testActivity->getDescription());
+		$this->assertEquals(new \DateTime('2011-10-10 10:00:00.0000+1:00'), $testActivity->getStarted());
+		$this->assertEquals('10.10.2011 10:00', $testActivity->getStartedFormatted());*/
+
+		// TODO Test `update`
+		// TODO Test `delete`
 		$this->markTestIncomplete();
 	}
 
-	public function testSelectRunningActivity()
-	{
-		$this->markTestIncomplete();
-	}
-
-	public function testSelectActivitiesForInterval()
-	{
-		$this->markTestIncomplete();
-	}
-
-	public function testStartActivity()
-	{
-		$this->markTestIncomplete();
-	}
-
-	public function testStopRunningActivity()
+	/**
+	 * Test mapper on MySQL database.
+	 */
+	public function testMysql()
 	{
 		$this->markTestIncomplete();
 	}
